f42442102c4a94a3ccbcb47e27431b040f1f2c22
ahamednadeem02 <111524475+ahamednadeem02@users.noreply.github.com>
2023-04-11 21:37:37 +0530

    show station table on webpage

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template
-import pandas
+import pandas as pd
 app = Flask(__name__)
 
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 
-@app.route("/")
+
+@app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
+    print("HI")
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
-            "temperature": temperature
-    }
+            "temperature": temperature}
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
